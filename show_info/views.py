26048575bbb55370fef05b4e8fedfbc969490908
bangbadak <bangbadak00@gmail.com>
2021-06-30 09:57:24 +0900

    6-30

--- show_info/views.py
+++ show_info/views.py
@@ -21,10 +21,28 @@ def index(request):
     # 'numOfRows': '10', 'pageNo': '1', 'dataType': 'JSON', 'dataCd': 'ASOS',
     # 'dateCd': 'DAY', 'startDt': '20200101', 'endDt': '20200102', 'stnIds': '108'}
     # response = requests.get(URL, params=params)
-    response = requests.get("http://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList?serviceKey=%2F08lfq3dASI1JUoPSrf2ic37mWQvgiER75rZmxvz8TYXj5w9uMkVM%2BgxyVFYI7WYjzG20CtdJ0FYUDtJEYVxQg%3D%3D&numOfRows=10&pageNo=1&dataCd=ASOS&dateCd=DAY&startDt=20100101&endDt=20100101&stnIds=108")
-    print(response.status_code)
+    response = requests.get("http://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList?serviceKey=%2F08lfq3dASI1JUoPSrf2ic37mWQvgiER75rZmxvz8TYXj5w9uMkVM%2BgxyVFYI7WYjzG20CtdJ0FYUDtJEYVxQg%3D%3D&numOfRows=10&pageNo=1&dataCd=ASOS&dateCd=DAY&startDt=20100101&endDt=20100101&stnIds=" + str(108))
     root = ElementTree.fromstring(response.content)
+    # arr = []
+    # cnt = 0
+    # for i in range(0, 205):
+    #     a = ElementTree.fromstring(requests.get("http://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList?serviceKey=%2F08lfq3dASI1JUoPSrf2ic37mWQvgiER75rZmxvz8TYXj5w9uMkVM%2BgxyVFYI7WYjzG20CtdJ0FYUDtJEYVxQg%3D%3D&numOfRows=10&pageNo=1&dataCd=ASOS&dateCd=DAY&startDt=20100101&endDt=20100101&stnIds=" + str(i + 90)).content)
+    #     if  a[0][1].text == "NO_DATA":
+    #         continue
+    #     elif a[0][1].text == "NORMAL_SERVICE":
+    #         arr[cnt] = a[1][1][0][1]
+    #         cnt += 1
+    #     else:
+    #         continue
 
+
+
+    # for i in range(0, 205):
+    #     a = ElementTree.fromstring(requests.get("http://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList?serviceKey=%2F08lfq3dASI1JUoPSrf2ic37mWQvgiER75rZmxvz8TYXj5w9uMkVM%2BgxyVFYI7WYjzG20CtdJ0FYUDtJEYVxQg%3D%3D&numOfRows=10&pageNo=1&dataCd=ASOS&dateCd=DAY&startDt=20100101&endDt=20100101&stnIds=" + str(i + 90)).content)
+    #     if a == None:
+    #         cnt += 1
+    #         continue
+    #     arr[i-cnt] = a[1][1][0][1]
     for avg in root[1][1][0]:
         print(avg.tag, avg.text)
 
